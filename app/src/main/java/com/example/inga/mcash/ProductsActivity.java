c487dfb7f59bd9edff72259b4b801be7f78de0f9
Marie92 <inga-hogrefe@web.de>
2015-03-11 13:08:07 +0100

    Layouts

--- app/src/main/java/com/example/inga/mcash/ProductsActivity.java
+++ app/src/main/java/com/example/inga/mcash/ProductsActivity.java
@@ -2,31 +2,26 @@ package com.example.inga.mcash;
 
 import android.app.Activity;
 import android.content.Intent;
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
 
-public class LoginActivity extends Activity {
-
+/**
+ * Created by Inga on 08.03.2015.
+ */
+public class ProductsActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_products);
 
-        Button buttonLogin = (Button) findViewById(R.id.button1);
+/*        Button buttonLogin = (Button) findViewById(R.id.button1);
         buttonLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(getApplicationContext(), ProductsActivity.class);
-                startActivity(intent);
+                Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
             }
-        });
+        });*/
     }
-
-
-
 }
